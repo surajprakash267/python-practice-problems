41a3818f484e09bd0d608a199e18ebc25a06cbdb
surajprakash267 <surajprakash267@gmail.com>
2025-10-11 14:38:11 +0530

    Take user input for name and age; print a greeting

---
+++ basics/02_input_output.py
@@ -0,0 +1,5 @@
+# Take user input for name and age; print a greeting
+
+x = input("Enter the name of the person :")
+y = int(input("Enter the age of the person :"))
+print(f"Hi {x}, welcome to the company. Guys for your kind information {x} has achieved this success in such a small age of {y}")
