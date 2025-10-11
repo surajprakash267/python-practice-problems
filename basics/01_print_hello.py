3a221fc3f5e8132fb7589c0525672b0975746156
surajprakash267 <surajprakash267@gmail.com>
2025-10-11 13:49:51 +0530

    Print “Hello, Python” without parentheses

---
+++ basics/01_print_hello.py
@@ -0,0 +1,5 @@
+#           Print “Hello, Python” without parentheses
+
+# Not possible in Python 3 — only works in Python 2
+# print "Hello, Python"  ← (Python 2)
+print("Hello, Python")   # ✅ Correct in Python 3
