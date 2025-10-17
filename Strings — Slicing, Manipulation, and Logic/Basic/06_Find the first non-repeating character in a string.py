4fbe108633e41fe602c3f4a0e18763a4a68a538d
surajprakash267 <surajprakash267@gmail.com>
2025-10-17 22:21:11 +0530

    Find first non-repeating character in a string

---
+++ Strings — Slicing, Manipulation, and Logic/Basic/06_Find the first non-repeating character in a string.py
@@ -0,0 +1,17 @@
+#           Find the first non-repeating character in a string.
+
+#Method : Using dictionary
+
+s = "swiss"
+freq = {}
+
+for ch in s:
+    freq[ch] = freq.get(ch, 0) + 1
+
+for ch in s:
+    if freq[ch] == 1:
+        print("First non-repeating character:", ch)
+        break
+
+# Answer : First non-repeating character: w
+
