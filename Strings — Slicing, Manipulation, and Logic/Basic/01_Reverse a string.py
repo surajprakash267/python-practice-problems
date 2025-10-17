e5022e4fec7888c2e7f92cdb7f728d0fbb063b75
surajprakash267 <surajprakash267@gmail.com>
2025-10-17 22:19:16 +0530

    Reverse a given string

---
+++ Strings — Slicing, Manipulation, and Logic/Basic/01_Reverse a string.py
@@ -0,0 +1,7 @@
+#.    Reverse a string ("hello" → "olleh").
+
+# A. Reverse a string
+# --------------------------------------------------
+a = "hello"
+rev_a = a[::-1]
+print(" Reversed string:", rev_a)
