a9e503a8b6ebcc5c12565a08d767c0431c1b0e33
surajprakash267 <surajprakash267@gmail.com>
2025-10-23 11:48:14 +0530

    Problem_04_Remove duplicates from list using set.

---
+++ 04_Tuples and Sets/Basic/04_Remove duplicates from list using set.py
@@ -0,0 +1,10 @@
+#✅ 4️⃣ Remove Duplicates from a List
+
+l1 = [2, 5, 7, 5, 9, 87, 54, 900, 0, 20]
+l2 = set(l1)
+print(list(l2))
+
+
+# ✔ Works great — duplicates are automatically removed.
+# 🟢 Note: The order may change, since sets are unordered.
+
