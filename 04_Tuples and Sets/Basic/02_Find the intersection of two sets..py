8b6bf8d8776e43e400350d892863872b6567b8ec
surajprakash267 <surajprakash267@gmail.com>
2025-10-23 11:47:22 +0530

    Problem_02_Find the intersection of two sets.

---
+++ 04_Tuples and Sets/Basic/02_Find the intersection of two sets..py
@@ -0,0 +1,14 @@
+#✅ 2️⃣ Intersection of Sets
+
+# The intersection of two sets # contains only the elements that are common to both sets.
+# Using the intersection() method
+
+l1 = {2, 5, 7, 9, 87, 54, 900, 0, 20}
+l2 = {4, 2, 9, 20}
+print(l1.intersection(l2))
+# OR
+print(l1 & l2)
+
+# ✔ Both methods work fine.
+# 🟢 Tip: & is faster, while .intersection() is more readable.
+
