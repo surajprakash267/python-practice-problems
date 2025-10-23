22aebf5fd3deb35a6fa16bc38a61701c622592d2
surajprakash267 <surajprakash267@gmail.com>
2025-10-23 11:48:41 +0530

    Problem_05_Check if a given element exists in a set.

---
+++ 04_Tuples and Sets/Basic/05_Check if a given element exists in a set.py
@@ -0,0 +1,7 @@
+x = int(input("Enter the element: "))
+l1 = {2, 5, 7, 9, 87, 54, 900, 0, 20}
+if x in l1:
+    print("The required data is present in the set.")
+else:
+    print("The required data is not present in the set.")
+
