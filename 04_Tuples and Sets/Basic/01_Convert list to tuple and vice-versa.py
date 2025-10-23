eb01926aa794ef0ec900955dcd0fdb65baddfa2d
surajprakash267 <surajprakash267@gmail.com>
2025-10-23 11:47:01 +0530

    Problem_01_Convert list to tuple and vice-versa.

---
+++ 04_Tuples and Sets/Basic/01_Convert list to tuple and vice-versa.py
@@ -0,0 +1,12 @@
+#✅ 1️⃣ Convert List ↔ Tuple
+
+l1 = [2, 5, 7, 5, 9, 87, 54, 900, 0, 20]
+l2 = tuple(l1)
+l3 = list(l2)
+print(l2)
+print(l3)
+
+
+# ✔ Works perfectly.
+# 🟢 Tip: This is useful when you want to make a list immutable (tuple) or editable again (list).
+
