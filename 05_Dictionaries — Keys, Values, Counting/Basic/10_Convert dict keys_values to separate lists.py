5cb8703855a2d00d501e092d5d9e328feba71832
surajprakash267 <surajprakash267@gmail.com>
2025-10-24 23:39:47 +0530

    Problem_10_Convert dict keys/values to separate lists

---
+++ 05_Dictionaries — Keys, Values, Counting/Basic/10_Convert dict keys_values to separate lists.py
@@ -0,0 +1,9 @@
+# ✅🔟 Convert dict keys/values to separate lists
+
+d = {'a': 10, 'b': 20, 'c': 30}
+
+keys_list = list(d.keys())
+values_list = list(d.values())
+
+print(keys_list)
+print(values_list)
