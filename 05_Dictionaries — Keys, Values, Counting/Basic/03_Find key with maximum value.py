94f3cf3bb632ba7cec7b161b2a32b219e23b644e
surajprakash267 <surajprakash267@gmail.com>
2025-10-24 23:37:00 +0530

    Problem_03_Find key with maximum value

---
+++ 05_Dictionaries — Keys, Values, Counting/Basic/03_Find key with maximum value.py
@@ -0,0 +1,10 @@
+#✅ 3️⃣ Find key with maximum value
+
+
+data = {'a': 15, 'b': 25, 'c': 10}
+max_key = max(data, key=data.get())
+print("Key with max value:", max_key)
+
+
+
+#🧠 key=data.get → compares based on the value.
